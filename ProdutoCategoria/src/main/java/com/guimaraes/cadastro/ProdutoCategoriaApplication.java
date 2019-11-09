d7c16cc6d0242fa8f1173d573ba64e28d139b6d5
Ivan Guimarães <iguimaraesf@gmail.com>
2019-11-09 13:11:52 -0200

    Primeiro upload;

---
+++ ProdutoCategoria/src/main/java/com/guimaraes/cadastro/ProdutoCategoriaApplication.java
@@ -0,0 +1,13 @@
+package com.guimaraes.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoCategoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoCategoriaApplication.class, args);
+	}
+
+}
